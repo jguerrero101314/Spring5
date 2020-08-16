37327508c12a9d1364a0e95ec56131a214dd76c1
Joel Guerrero <joelg1014@hotmail.com>
2020-08-16 08:38:04 -0700

    12. Pasar datos a la vista

--- workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,7 +1,12 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,8 +17,16 @@ public class IndexController {
 	//@GetMapping(value="/index")
 	//@GetMapping("/index")
 	  @GetMapping({"/index", "/", "/home"})
-	 public String index() {
-		
+	 public String index(Model model) {
+		  //Model model
+		 // ModelMap model
+		  //Map<String, Object> map
+		  model.addAttribute("titulo","Hola Spring Framework!");		
+		  //map.put("titulo","Hola Spring Framework con Map!");	
+		 // mv.addObject("titulo","Hola Spring Framework con ModelAndView!");	
+		 // mv.setViewName("index");
+		  
+		//  return mv;
 		return "index";
 	}
 
