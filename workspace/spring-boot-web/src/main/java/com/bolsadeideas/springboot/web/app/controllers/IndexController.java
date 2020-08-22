a4d6a4a82d46eb3508df5d843bf543fb4c4bc227
Joel Guerrero <joelg1014@hotmail.com>
2020-08-22 09:38:08 -0700

    * Recursos de Spring MVC y la Inyección de Dependencia * 18. Anotación @ModelAttribute * 17. Directiva each (foreach) de Thymeleaf

--- workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,16 +1,24 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.bolsadeideas.springboot.web.app.model.Usuario;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
 	//@RequestMapping(value="/index", method=RequestMethod.GET)
@@ -21,7 +29,7 @@ public class IndexController {
 		  //Model model
 		 // ModelMap model
 		  //Map<String, Object> map
-		  model.addAttribute("titulo","Hola Spring Framework!");		
+		  model.addAttribute("titulo","Hola Spring Framework con model!");		
 		  //map.put("titulo","Hola Spring Framework con Map!");	
 		 // mv.addObject("titulo","Hola Spring Framework con ModelAndView!");	
 		 // mv.setViewName("index");
@@ -29,5 +37,44 @@ public class IndexController {
 		//  return mv;
 		return "index";
 	}
+	  @RequestMapping("/perfil")
+	  public String perfil(Model model) {
+		  
+		  Usuario usuario = new Usuario();
+		  usuario.setNombre("Joel");
+		  usuario.setApellido("Guerrero");
+		  usuario.setEmail("joelg104@hotmail.com");
+		  
+		  model.addAttribute("usuario", usuario );
+		  model.addAttribute("titulo","Perfil del usuario: ".concat(usuario.getNombre() + " " + usuario.getApellido()));	
+		  
+		  return "perfil";
+		  
+	  }
+	  @RequestMapping("/listar")
+	  public String listar(Model model) {
+		 // List<Usuario> usuarios = new ArrayList<>();
+		/*  List<Usuario> usuarios = Arrays.asList(
+				        new Usuario("Joel","Guerrero", "jguerrero10@hotmail.com"),
+				        new Usuario("Jose","Guevara", "jguevara@hotmail.com"),
+				        new Usuario("Luis","Robinson", "lrobinson@outlook.com"),
+		        new Usuario("Tornado","Roe", "roe@outlook.com"));	*/	
+		  
+		/*  usuarios.add(new Usuario("Jose","Guevara", "jguevara@hotmail.com"));
+		  usuarios.add(new Usuario("Luis","Robinson", "lrobinson@outlook.com"));*/
+		  model.addAttribute("titulo", "Listado de usuarios" );
+		 // model.addAttribute("usuarios", usuarios);
+		  return "listar";
+	  }
+	  @ModelAttribute("usuarios")
+	  public  List<Usuario> poblarUsuarios(){
+		  List<Usuario> usuarios = Arrays.asList(
+			        new Usuario("Joel","Guerrero", "jguerrero10@hotmail.com"),
+			        new Usuario("Jose","Guevara", "jguevara@hotmail.com"),
+			        new Usuario("Luis","Robinson", "lrobinson@outlook.com"),
+			        new Usuario("Tornado","Roe", "roe@outlook.com"));
+		  
+		  return usuarios;	  
+	  }
 
 }
