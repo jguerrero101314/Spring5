fbe328430d9edf5f420f72a2c2a5741eca35e0cd
Joel Guerrero <joelg1014@hotmail.com>
2020-08-23 09:42:36 -0700

    20. Obtener varios parámetros de la URL con @RequestParam

--- workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
+++ workspace/spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import javax.servlet.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +17,27 @@ public class EjemploParamsController {
 	
 	@GetMapping("/string")
 	//public String param(@RequestParam(name="texto"), Model model)
-	public String param(@RequestParam(name="texto", required=false, defaultValue= "no ingreso ningun texto") String texto, Model model) {
+	public String param(@RequestParam(name="texto", required=false, defaultValue= "no ingreso ningun texto") String texto, Model model) {		
+		model.addAttribute("resultado", "El texto enviado es: " + texto);		
+		return "params/ver";
+	}
+	/*@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo,@RequestParam Integer numero, Model model) {		
+		model.addAttribute("resultado", "El saludo enviado es: " + saludo + "'y el numero es '" + numero + "'");		
+		return "params/ver";
+	}*/
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {	
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+		try {
+		  numero = Integer.parseInt(request.getParameter("numero"));
+		}catch(NumberFormatException e) {
+			numero = 0;
+		}
 		
-		model.addAttribute("resultado", "El texto enviado es: " + texto);
-		
-		
+		model.addAttribute("resultado", "El saludo enviado es: " + saludo + "'y el numero es '" + numero + "'");		
 		return "params/ver";
 	}
 
